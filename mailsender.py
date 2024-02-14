0cbf1c5f7824a9195cf546114b3a8a6f593ef825
Joram Bosire <jbosire@vassar.edu>
2024-02-13 21:38:06 -0500

    Finalize emails

--- mailsender.py
+++ mailsender.py
@@ -22,7 +22,7 @@ def fetchdata():
     spreadsheet = client.open_by_key(spreadsheet_id)
 
     worksheet = spreadsheet.get_worksheet(
-        2
+        1
     )  # Assuming the data is in the first worksheet (index 0)
     # Get the values from the specified column (column C starting from row 2)
  
@@ -30,14 +30,14 @@ def fetchdata():
     recipients = worksheet.col_values(3)[1:]
     recipient_emails = worksheet.col_values(4)[1:]
     sender_emails = worksheet.col_values(8)[1:]
-    codes = worksheet.col_values(10)[1:]
+    codes = worksheet.col_values(9)[1:]
     return (senders, recipients, recipient_emails, sender_emails, codes)
 
 
 
 
 # Email configuration
-def sendmail(recipient_email, recipient_name, sender=True, code=None):
+def sendmail(recipient_email, recipient_name, receiver_name="", sender=True, code=None):
     sender_email = os.getenv("EMAIL")
     password = os.getenv("EMAIL_PASSWORD")  # Access password stored in an environment variable
     subject = "[Valentine’s++] A Heartfelt Surprise Awaits You! 🌟💌"
@@ -45,32 +45,30 @@ def sendmail(recipient_email, recipient_name, sender=True, code=None):
 
     if sender:
         body = f"""Dear {recipient_name},
-We hope this email finds you with a heart full of anticipation! Your Valentine's++ gift package has been lovingly prepared and sent to your special someone. 💖
+<p>We hope this email finds you with a heart full of anticipation! Your Valentine's++ gift package has been lovingly prepared and sent to {receiver_name}. 💖</p>
 
-We wanted to let you know that everything is set for a day full of smiles, sweetness, and surprises. Please gently remind your loved one to visit VC++ booth in College Center, Main Building between 11AM and 5PM to pick up their gift package. It's a beautiful gesture that's sure to make their day even more memorable. 🎁✨
+<p>We wanted to let you know that everything is set for a day full of smiles, sweetness, and surprises. Please gently remind your loved one to visit VC++ booth in College Center, Main Building between 11AM and 5PM to pick up their gift package. It's a beautiful gesture that's sure to make their day even more memorable. 🎁✨</p>
 
-Thank you for choosing to spread love with Valentine's++. Your thoughtfulness is what makes this event so special. (＾-＾)v
+<p>Thank you for choosing to spread love with Valentine's++. Your thoughtfulness is what makes this event so special. (＾-＾)v</p>
 
-Warmly,
+<p>Warmly,</p>
 
-The VC++ Team
+<p>The VC++ Team</p>
         """
     else:
         body = f"""Dear {recipient_name},
 
-Exciting news! Someone special has sent you a Valentine's++ gift package, filled with love and thoughtfulness just for you. 💕
+<p>Exciting news! Someone special has sent you a Valentine's++ gift package, filled with love and thoughtfulness just for you. 💕</p>
 
-To discover what awaits, please visit VC++’s booth in College Center, Main Building between 11AM and 5PM to pick up your surprise package. But that's not all – a unique code has been created for you to access a heartfelt message and your very own Valentine's card online. Simply visit {WEBSITE_URL} and enter the code {code} to unveil the love sent your way. 💻❤️
+<p>To discover what awaits, please visit VC++’s booth in College Center, Main Building between 11AM and 5PM to pick up your surprise package. But that's not all – a unique code has been created for you to access a heartfelt message and your very own Valentine's card online. Simply visit <a href="{WEBSITE_URL}">www.valentinesplusplus.com</a> and enter the code <strong>{code}</strong> to unveil the love sent your way. 💻❤️</p>
 
-And there's a cherry on top! Don't miss out on our special Valentine's Theme Boba Tea – Strawberry Green Tea infused with Strawberry Popping Bubble and Fruit Jelly. It's the perfect treat to make your day even sweeter. 🍓🍵😊
+<p>We can't wait to see you and share in this moment of joy and sweetness. Remember, love is in the air, and it's all for you!</p>
 
-We can't wait to see you and share in this moment of joy and sweetness. Remember, love is in the air, and it's all for you!
+<p>With all our hearts,</p>
 
-With all our hearts,
+<p>The VC++ Team</p>
 
-The VC++ Team
-
-P.S. Don't forget to bring your smile! (＾▽＾)
+<p>P.S. Don't forget to bring your smile! (＾▽＾)</p>
 
 """
 
@@ -78,7 +76,7 @@ P.S. Don't forget to bring your smile! (＾▽＾)
     message["From"] = sender_email
     message["To"] = recipient_email
     message["Subject"] = subject
-    message.attach(MIMEText(body, "plain"))
+    message.attach(MIMEText(body, "html"))
 
     try:
         server = smtplib.SMTP("smtp.gmail.com", 587)
@@ -87,18 +85,19 @@ P.S. Don't forget to bring your smile! (＾▽＾)
         server.login(sender_email, password)
 
         server.sendmail(sender_email, recipient_email, message.as_string())
+        print("Email sent successfully")
 
     except Exception as e:
-        print(f"Failed to send email. Error: {str(e)}")
+        if sender:
+            print(f"Failed to send email to sender {recipient_email} name {recipient_name} other person {receiver_name}. Error: {str(e)}")
+        else:
+            print(f"Failed to send email to receiver {recipient_email} name {recipient_name} other person {receiver_name}. Error: {str(e)}")
     finally:
         server.quit()
 
 
 if __name__ == "__main__":
     senders, recipients, recipient_emails, sender_emails, codes = fetchdata()
-    
-
-
     for i in range(len(senders)):
         sender = senders[i]
         recipient = recipients[i]
@@ -107,9 +106,11 @@ if __name__ == "__main__":
         code = codes[i]
        
         # send to reciever of message
-        sendmail(recipient_email, recipient, False, code)
+        # if "@" in recipient_email:
+        #     sendmail(recipient_email, recipient, sender=False, code=code)
 
         # send to sender of message
-        sendmail(sender_email, sender)
+        # if "@" in sender_email:
+        #     sendmail(sender_email, sender, receiver_name=recipient)
 
    
