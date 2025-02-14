c803145f0183323d64cf5b1008e873e7b6e5117d
vzhang03 <vzhang@vassar.edu>
2025-02-13 19:38:50 -0500

    Writing documents from spreadsheet correctly

--- backend/databaseWrite.js
+++ backend/databaseWrite.js
@@ -31,9 +31,7 @@ async function insertDocument(collection, document) {
     console.log("Document inserted successfully!", insertResult.insertedId);
   } catch (error) {
     console.error("Error inserting document:", error);
-  } finally {
-    await client.close();
-  }
+  } 
 }
 
 function mapColumnsNames(document){
@@ -83,23 +81,10 @@ const main = async () => {
       .on("end", () => resolve(data))
       .on("error", (err) => reject(err));
   });
-
-  // console.log(results);
-
-
-  // const document = {
-  //   Timestamp: new Date().toISOString(),
-  //   "Email Address": "jlui@vassar.edu",
-  //   "Write your first and last name or leave it blank if you'd like to stay anonymous!": "Sujaq",
-  //   "Who are we sending this gift to? (Recipient’s name)": "Sujaq",
-  //   "Recipient’s email address": "Suhuq@vassar.edu",
-  //   "Would you like to add a message? (optional)": "I bet you’re hungry. Fatty.",
-  //   "Choose an option for your card!": "Option 2",
-  //   "Would you like to add roses and a chocolate gift package for $5?": "No"
-  // };
   
-  // Writing to database
-  // await insertDocument(collection, document);
+  for (const row of results){
+    await insertDocument(collection, row);
+  }
 
   await client.close();
 }
