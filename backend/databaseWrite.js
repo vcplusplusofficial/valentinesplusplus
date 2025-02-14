9cb58b77098cef6b99d6beac122b522f11d932f8
vzhang03 <vzhang@vassar.edu>
2025-02-14 00:11:54 -0500

    added one more todo

--- backend/databaseWrite.js
+++ backend/databaseWrite.js
@@ -18,6 +18,7 @@ async function connectToDatabase() {
   try {
     await client.connect();
     console.log("Connected to MongoDB!");
+    // TODO: update this with production when writing
     return client.db("sampleDatabase").collection("testDatabaseWrite");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
